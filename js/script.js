92d180130145e3c730a684c4a273082b999eb801
jyrgalbek04 <jyldyzbekuulujyrgalbek@gmail.com>
2021-11-29 16:53:42 +0600

    initial

--- js/script.js
+++ js/script.js
@@ -25,24 +25,19 @@ blockClose.addEventListener("click", () => {
     document.querySelector(".row--title__block").style.display = "none"
 })
 
-
-const swiper = new Swiper(".mySwipergit ", {
+const swiper = new Swiper(".mySwiper", {
     effect: "coverflow",
     grabCursor: true,
     centeredSlides: true,
     slidesPerView: "auto",
     coverflowEffect: {
-        rotate: 10,
+        rotate: 50,
         stretch: 0,
-        depth: 50,
-        modifier: 8,
+        depth: 100,
+        modifier: 1,
         slideShadows: true,
-        autoplay: {
-            delay: 5000,
-        },
     },
     pagination: {
         el: ".swiper-pagination",
     },
 });
-swiper()
